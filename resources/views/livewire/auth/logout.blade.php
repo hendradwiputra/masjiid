c59f1e6653355952df5d783f4bb2703cb89ffc86
hendradwiputra <hendra.doank@gmail.com>
2025-07-27 12:21:14 +0700

    Update logout.blade.php

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,5 +1,20 @@
 <div>
-    <button wire:click="logout" class="block w-full text-left px-4 py-2 text-sm text-slate-700 hover:bg-stone-200 rounded-md">
+    <button wire:click="logout" class="flex items-center w-full text-left px-2 py-2 gap-2 text-sm hover:bg-blue-100">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#000000"
+            stroke-width="1.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-5 w-5"
+        >
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M9 12h12l-3 -3" />
+            <path d="M18 15l3 -3" />
+        </svg>
         Sign out
     </button>
+    
 </div>
