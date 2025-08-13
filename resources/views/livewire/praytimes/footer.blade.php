f96d3e3d180083b5cf8b0b48bd798969683419f0
hendradwiputra <hendra.doank@gmail.com>
2025-08-13 21:07:43 +0700

    Fix layout issue in smaller screen

--- resources/views/livewire/praytimes/footer.blade.php
+++ resources/views/livewire/praytimes/footer.blade.php
@@ -1,58 +1,52 @@
-            
-            <div class="bg-gradient-to-t from-slate-900 to bg-slate-600">
-                <div class="mx-auto w-full">
-                    <div class="font-mavenpro">
+<div class="bg-gradient-to-t from-slate-900 to bg-slate-600">
+    <div class="mx-auto w-full">
+        <div class="font-mavenpro">
 
-                        <!-- Running Text -->
-                        <div id="running-text">
-                            <div class="flex">
-                                <div class="text-white text-xl lg:text-6xl uppercase" id="running-text-content"></div>
-                            </div>
-                        </div>  
-                        <!-- End of running-text -->
-                        
-                        <!-- Copyright -->
-                        <div id="copyright">
-                            <div class="w-full bg-gradient-to-l from-gray-900 to bg-gray-500">
-                                <div class="sm:flex items-center justify-between mx-1">
-                                    <p class="text-white text-xs tracking-wide">
-                                        &copy; Copyright {{ now()->year}} <strong>imasjidpro</strong>. All rights reserved.
-                                    </p>
-                                    <div class="flex gap-4 font-mavenpro">
-                                        <ul class="flex flex-col">
-                                            <li class="flex" id="contact_no">
-                                                
-                                            </li>                        
-                                        </ul>
-                                        <ul class="flex flex-col">
-                                            <li class="flex">
-                                                <span>
-                                                    <a href="/dashboard" target="_blank" class="inline-flex items-center text-white text-xs tracking-wide space-x-1 ml-1">                                                      
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            width="16"
-                                                            height="16"
-                                                            viewBox="0 0 24 24"
-                                                            fill="none"
-                                                            stroke="#fff"
-                                                            stroke-width="1"
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                        >
-                                                            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
-                                                            <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                                                        </svg>
-                                                        <span>Settings</span>
-                                                    </a>
-                                                </span>
-                                            </li>                        
-                                        </ul>
-                                    </div>                                    
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End of copyright -->
-                        
-                    </div>
-                </div>                
+            <!-- Running Text -->
+            <div id="running-text">
+                <div class="flex">
+                    <div class="text-white text-xl lg:text-6xl uppercase" id="running-text-content"></div>
+                </div>
             </div>
+            <!-- End of running-text -->
+
+            <!-- Copyright -->
+            <div id="copyright">
+                <div class="w-full bg-gradient-to-l from-gray-900 to bg-gray-500">
+                    <div
+                        class="flex flex-col items-center justify-center sm:flex-row sm:justify-between mx-1 text-center sm:text-left">
+                        <p class="text-white text-xs md:text-sm tracking-wide">
+                            &copy;{{ now()->year}} <strong>Masjiid</strong>. All rights reserved.
+                        </p>
+                        <div class="flex gap-4 font-mavenpro">
+                            <ul class="flex flex-col">
+                                <li class="flex" id="contact_no">
+
+                                </li>
+                            </ul>
+                            <ul class="flex flex-col">
+                                <li class="flex">
+                                    <span>
+                                        <a href="/dashboard" target="_blank"
+                                            class="inline-flex items-center text-white text-xs tracking-wide space-x-1 ml-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1"
+                                                stroke-linecap="round" stroke-linejoin="round">
+                                                <path
+                                                    d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                                                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                            </svg>
+                                            <span class="text-xs md:text-sm">Settings</span>
+                                        </a>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End of copyright -->
+
+        </div>
+    </div>
+</div>
