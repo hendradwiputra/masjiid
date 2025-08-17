a3478afb9ae13d11e20c78776d1146b5ffe6da59
hendradwiputra <hendra.doank@gmail.com>
2025-08-17 17:07:22 +0700

    Update dashboard.blade.php

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,8 +1,6 @@
-@extends('livewire.layouts.content')
-
-@section('content')
-    
-
-    <h1>Welcome, {{ Auth::user()->name }}</h1>
-
-@endsection
+<x-layouts.content>
+    <div class="flex items-center">
+        <img src="{{ 'storage/images/icon/point.png' }}" class="h-5" alt="logo">
+        <h1 class="text-xl font-semibold text-gray-800 mb-6 mt-6">Dashboard</h1>
+    </div>
+</x-layouts.content>
