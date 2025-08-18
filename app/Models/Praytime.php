e013c6551c16a3785b202c6780fad2f943cf7da8
hendradwiputra <hendra.doank@gmail.com>
2025-08-18 21:17:42 +0700

    Update Praytime.php

--- app/Models/Praytime.php
+++ app/Models/Praytime.php
@@ -10,7 +10,7 @@ class Praytime extends Model
         'time_format', 'prayer_calc_method', 'latitude', 'longitude', 'dst', 'timezone', 'dst', 'hijri_correction',
         'prayer1_alias', 'prayer2_alias', 'prayer3_alias', 'prayer4_alias', 'prayer5_alias', 'prayer6_alias',
         'sunrise_lock_duration', 'prayer_lock_duration', 'jumuah_lock_duration', 'sunrise_caption', 
-        'prayer_caption', 'adhan_caption', 'iqomah_caption', 'adhan_duration',
+        'prayer_caption', 'jumuah_caption','before_adhan_caption', 'adhan_caption', 'iqomah_caption', 'adhan_duration',
         'prayer1_iqomah_duration', 'prayer3_iqomah_duration', 'prayer4_iqomah_duration', 'prayer5_iqomah_duration', 
         'prayer6_iqomah_duration', 'prayer1_correction', 'prayer2_correction', 'prayer3_correction',
         'prayer4_correction', 'prayer5_correction', 'prayer6_correction'
