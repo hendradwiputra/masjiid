5ec0b49728cae9b36c66bb97c657561b195a9653
hendradwiputra <hendra.doank@gmail.com>
2025-09-09 16:03:16 +0700

    Update Timer.php

--- app/Livewire/Praytimes/Timer.php
+++ app/Livewire/Praytimes/Timer.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Praytimes;
 
 use App\Models\Praytime;
+use App\Models\Notification;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
@@ -10,16 +11,22 @@ use Livewire\Attributes\Title;
 #[Title('Masjiid App - Timer')]
 class Timer extends Component
 {
-    public function render()
+    protected function getNotification()
     {
-        $praytimes = $this->getPrayTimes();
+        $notification = Notification::first();
 
-        return view('livewire.praytimes.timer', compact('praytimes'));
+        return [
+            'sunrise_caption' => $notification->sunrise_caption,
+            'prayer_caption' => $notification->prayer_caption,
+            'before_adhan_caption' => $notification->before_adhan_caption,
+            'adhan_caption' => $notification->adhan_caption,            
+            'iqomah_caption' => $notification->iqomah_caption,
+            'jumuah_caption' => $notification->jumuah_caption
+        ];
     }
 
     protected function getPraytimes()
     {
-
         // Get the first profile from the database
         $praytimes = Praytime::first();
 
@@ -28,13 +35,16 @@ class Timer extends Component
             'sunrise_lock_duration' => $praytimes->sunrise_lock_duration,
             'prayer_lock_duration' => $praytimes->prayer_lock_duration,
             'jumuah_lock_duration' => $praytimes->jumuah_lock_duration,
-            'adhan_duration' => $praytimes->adhan_duration,
-            'sunrise_caption' => $praytimes->sunrise_caption,
-            'prayer_caption' => $praytimes->prayer_caption,
-            'before_adhan_caption' => $praytimes->before_adhan_caption,
-            'adhan_caption' => $praytimes->adhan_caption,            
-            'iqomah_caption' => $praytimes->iqomah_caption,
-            'jumuah_caption' => $praytimes->jumuah_caption,
+            'adhan_duration' => $praytimes->adhan_duration            
         ];
     }
+
+    public function render()
+    {
+        $praytimes = $this->getPrayTimes();
+        $notification = $this->getNotification();
+
+        return view('livewire.praytimes.timer', compact('praytimes', 'notification'));
+    }
+
 }
