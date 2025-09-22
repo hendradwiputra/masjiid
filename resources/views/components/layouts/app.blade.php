47cc2af01b0aa706a4c7f537ff662c4cfca81b4f
hendradwiputra <hendra.doank@gmail.com>
2025-09-22 11:04:08 +0700

    remove preloader

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,19 +12,6 @@
 </head>
 
 <body>
-
-    <!-- ===== Preloader Start ===== -->
-    <div x-data="{ loaded: true }">
-        <div x-show="loaded" x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0" x-init="setTimeout(() => { loaded = false }, 100)"
-            class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-gradient-to-b from-stone-600 to bg-stone-400">
-            <div
-                class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-amber-400 border-t-transparent">
-            </div>
-        </div>
-    </div>
-    <!-- ===== Preloader End ===== -->
-
     {{ $slot }}
     @livewireScripts
 </body>
