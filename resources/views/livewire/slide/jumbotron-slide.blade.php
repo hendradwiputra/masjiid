818e412602c88247ecffcda95725c1c9baa4ef80
hendradwiputra <hendra.doank@gmail.com>
2025-09-23 12:00:04 +0700

    Update jumbotron-slide.blade.php

--- resources/views/livewire/slide/jumbotron-slide.blade.php
+++ resources/views/livewire/slide/jumbotron-slide.blade.php
@@ -4,17 +4,13 @@
 
     <div id="app-config" style="display: none;" data-random-images='@json($randomImages ?? [])'></div>
 
-    <div id="hero_section" class="absolute inset-0 bg-gradient-to-b from-gray-900 to bg-gray-400">
-        @if(!empty($randomImages))
-        <img data-aos="fade-up" data-aos-duration="1000"
-            src="{{ $randomImages[0] ?? asset('storage/images/upload/default-image.jpg') }}"
-            alt="Random Background Image" id="random-image" class="w-full h-full object-cover object-center">
-        @endif
-        <div class="absolute inset-0 bg-stone-700 opacity-30"></div>
+    <!-- Jumbotron section -->
+    <div id="jumbotron-section">
+        @include('livewire.praytimes.partials.hero')
     </div>
 </div>
+</div>
 
-<script src="{{ asset('storage/dist/jquery/jquery-3.7.1.min.js') }}"></script>
 <script src="{{ asset('storage/dist/imageRandomizer/imageRandomizer.js') }}"></script>
 
 <script>
