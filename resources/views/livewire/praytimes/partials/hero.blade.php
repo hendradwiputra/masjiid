55a2c41e986b4d22c48f789f82417d39885472bb
hendradwiputra <hendra.doank@gmail.com>
2025-09-24 21:04:49 +0700

    Update hero.blade.php

--- resources/views/livewire/praytimes/partials/hero.blade.php
+++ resources/views/livewire/praytimes/partials/hero.blade.php
@@ -3,5 +3,5 @@
     <img src="{{ $randomImages[0] ?? asset('storage/images/upload/default-image.jpg') }}" alt="Random Background Image"
         id="random-image" class="w-full h-full object-cover object-center transition-opacity duration-700 ease-in-out">
     @endif
-    <div class="absolute inset-0 bg-stone-700 opacity-30"></div>
+    <div class="absolute inset-0 bg-stone-700 opacity-10"></div>
 </div>
