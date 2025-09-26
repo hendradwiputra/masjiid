ba1c3634e9bc2288a2da1470da53116592823928
hendradwiputra <hendra.doank@gmail.com>
2025-09-26 15:18:25 +0700

    remove dashboard

--- resources/views/livewire/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layouts.content>
-    <div class="flex items-center">
-        <img src="{{ 'storage/images/icon/point.png' }}" class="h-5" alt="logo">
-        <h1 class="text-xl font-semibold text-gray-800 mb-6 mt-6">Dashboard</h1>
-    </div>
-</x-layouts.content>
