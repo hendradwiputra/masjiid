4ad3eb091d55c4773568a1450afb36216c1fbeb9
hendradwiputra <hendra.doank@gmail.com>
2025-10-11 13:26:39 +0700

    Update clock.blade.php

--- resources/views/livewire/praytimes/partials/clock.blade.php
+++ resources/views/livewire/praytimes/partials/clock.blade.php
@@ -1,10 +1,7 @@
-<div class="flex flex-row bg-gradient-to-r from-stone-400 to bg-stone-200">
-    <h1 class="font-montserrat text-5xl lg:text-8xl font-bold text-gray-800 text-shadow-lg mx-2" id="clock">
-    </h1>
-    <div class="flex-row mr-2">
-        <h4 class="font-montserrat font-bold text-teal-800 text-2xl lg:text-5xl text-shadow-lg lg:pt-2 tabular-nums"
-            id="seconds"></h4>
-        <h4 class="font-montserrat font-bold text-teal-800 text-base lg:text-4xl text-shadow-lg leading-1 lg:leading-5"
-            id="ampm"></h4>
-    </div>
+<h1 class="font-montserrat text-5xl lg:text-8xl font-bold text-gray-800 text-shadow-lg mx-2" id="clock"></h1>
+<div class="flex-row mr-2">
+    <h4 class="font-montserrat font-bold text-teal-800 text-2xl lg:text-5xl text-shadow-lg lg:pt-2 tabular-nums"
+        id="seconds"></h4>
+    <h4 class="font-montserrat font-bold text-teal-800 text-base lg:text-4xl text-shadow-lg leading-1 lg:leading-5"
+        id="ampm"></h4>
 </div>
