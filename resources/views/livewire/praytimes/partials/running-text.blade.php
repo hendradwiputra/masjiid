7a3cb8447a4ed6cd60c4fb11566121fb27071fde
hendradwiputra <hendra.doank@gmail.com>
2025-10-11 13:27:08 +0700

    Update running-text.blade.php

--- resources/views/livewire/praytimes/partials/running-text.blade.php
+++ resources/views/livewire/praytimes/partials/running-text.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="p-2 left-0 w-full overflow-hidden bg-gradient-to-l from-slate-900 to bg-slate-700 font-mavenpro text-stone-100 text-xl lg:text-5xl font-medium whitespace-nowrap z-50">
+    class="p-2 left-0 w-full overflow-hidden bg-gradient-to-l from-gray-950 to bg-gray-800 font-mavenpro text-stone-100 text-xl lg:text-5xl font-medium whitespace-nowrap z-50">
     <div id="running-text" class="flex animate-scroll">
         {{ $tickerText }}
     </div>
