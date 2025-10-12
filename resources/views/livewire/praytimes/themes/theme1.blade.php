5b27544c1f9f90d23b35accff5058e82af315dd4
hendradwiputra <hendra.doank@gmail.com>
2025-10-12 11:23:22 +0700

    Update theme1.blade.php

--- resources/views/livewire/praytimes/themes/theme1.blade.php
+++ resources/views/livewire/praytimes/themes/theme1.blade.php
@@ -2,7 +2,11 @@
 <div class="fixed left-0 inset-x-0 top-0 z-50 transition-all duration-700 ease-in-out">
     <div id="date-section">
         <!-- Current Date -->
-        @include('livewire.praytimes.partials.currentdate')
+        <div id="datetime_section" class="bg-gradient-to-l from-stone-800 to bg-stone-400">
+            <div class="mx-auto w-full py-1 px-1">
+                @include('livewire.praytimes.partials.currentdate')
+            </div>
+        </div>
     </div>
 
     <div id="profile-section"
@@ -11,9 +15,26 @@
         @include('livewire.praytimes.partials.profile-logo-left')
     </div>
 
-    <div id="nextprayer-section" class="z-50 fixed right-1 pt-1 lg:pt-1">
+    <div id="nextprayer-section" class="z-50 fixed right-1 pt-2 lg:pt-2">
         <!-- Next Prayer -->
-        @include('livewire.praytimes.partials.nextprayer')
+        <div class="flex items-center">
+            <div class="bg-gradient-to-b from-teal-500 to-teal-700 outline-2 outline-stone-50 py-1 px-3 rounded-l-full">
+                <svg viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="animate-pulse h-7 lg:h-20 max-w-[30px] max-h-[30px] lg:max-w-[40px] lg:max-h-[48px]">
+                    <path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5" />
+                    <path d="M5.63 7.16l0 .01" />
+                    <path d="M4.06 11l0 .01" />
+                    <path d="M4.63 15.1l0 .01" />
+                    <path d="M7.16 18.37l0 .01" />
+                    <path d="M11 19.94l0 .01" />
+                </svg>
+            </div>
+            <div
+                class="font-montserrat font-medium text-gray-800 text-xl lg:text-5xl tracking-tighter tabular-nums inline-flex flex-wrap items-center justify-center gap-x-2 bg-gradient-to-b from-stone-50 to-stone-300 outline-2 outline-stone-50 shadow-xl py-1 px-3 rounded-r-full whitespace-nowrap">
+                @include('livewire.praytimes.partials.nextprayer')
+            </div>
+        </div>
     </div>
 </div>
 <!-- End of header -->
