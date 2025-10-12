c9c935b8f04412540cd61547246f15939d760d5e
hendradwiputra <hendra.doank@gmail.com>
2025-10-12 11:23:34 +0700

    Update currentdate.blade.php

--- resources/views/livewire/praytimes/partials/currentdate.blade.php
+++ resources/views/livewire/praytimes/partials/currentdate.blade.php
@@ -1,8 +1,4 @@
-<div id="datetime_section" class="bg-gradient-to-l from-stone-800 to bg-stone-400">
-    <div class="mx-auto w-full py-1 px-1">
-        <div class="flex flex-col">
-            <p class="font-mavenpro font-semibold text-stone-200 text-md lg:text-2xl text-right text-shadow-lg highlight-me mr-1"
-                id="current_date"></p>
-        </div>
-    </div>
+<div class="flex flex-col">
+    <p class="font-mavenpro font-semibold text-stone-200 text-md lg:text-2xl text-right text-shadow-lg highlight-me mr-1"
+        id="current_date"></p>
 </div>
