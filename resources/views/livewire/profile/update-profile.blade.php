9bf1230c93417767f7f317a1954fd9317060e0e8
hendradwiputra <hendra.doank@gmail.com>
2025-10-13 17:51:32 +0700

    Update update-profile.blade.php

--- resources/views/livewire/profile/update-profile.blade.php
+++ resources/views/livewire/profile/update-profile.blade.php
@@ -20,7 +20,7 @@
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div class="space-y-6">
-                    <div class="border border-gray-200 rounded-2xl">
+                    <div class="border border-gray-200 rounded-2xl shadow-2xl">
                         <div class="px-4 py-5 bg-stone-100 rounded-t-2xl">
                             <div class="flex items-center">
                                 <svg class="h-6 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -64,7 +64,7 @@
                 </div>
 
                 <div class="space-y-6">
-                    <div class="border border-gray-200 rounded-2xl">
+                    <div class="border border-gray-200 rounded-2xl shadow-2xl">
                         <div class="px-4 py-5 bg-stone-100 rounded-t-2xl">
                             <div class="flex items-center">
                                 <svg class="h-6 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -112,7 +112,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="border border-gray-200 rounded-2xl">
+                    <div class="border border-gray-200 rounded-2xl shadow-2xl">
                         <div class="px-4 py-5 bg-stone-100 rounded-t-2xl">
                             <div class="flex items-center">
                                 <svg class="h-6 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -147,7 +147,7 @@
 
             </div>
 
-            <div class="px-2 py-2 mt-3 border border-gray-200 bg-stone-100">
+            <div class="px-2 py-2 mt-3 border border-gray-200 bg-stone-100 shadow-2xl">
                 <div class="flex justify-between">
                     <div class="flex items-center">
                         <svg class="h-4 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25"
@@ -184,33 +184,32 @@
     <div x-data="{ showImageModal: @entangle('showImageModal') }" x-show="showImageModal" x-cloak
         class="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
         <div class="bg-white rounded-lg p-6 w-full max-w-4xl">
-            <div class="flex justify-between items-center mb-4">
-                <h2 class="text-lg font-semibold">Pilih Logo</h2>
-                <button @click="showImageModal = false" wire:click="closeImageModal"
-                    class="text-gray-500 hover:text-gray-700">
-                    <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M18 6L6 18" />
-                        <path d="M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-            @if ($images->count())
-            <div class="grid grid-cols-4 md:grid-cols-4 gap-4">
-                @foreach ($images as $image)
-                <div class="border border-gray-200 rounded p-1 shadow cursor-pointer"
-                    wire:click="selectImage({{ $image->id }})">
-                    <img src="{{ asset('storage/' . $image->image_name) }}" alt="{{ $image->category }} image"
-                        class="bg-stone-700 max-w-full h-auto object-cover rounded hover:bg-stone-400">
+            <div class="overflow-y-auto max-h-[80vh]">
+                <div class="flex justify-between items-center mb-4">
+                    <h2 class="text-lg font-semibold">Pilih Logo</h2>
+                    <button @click="showImageModal = false" wire:click="closeImageModal"
+                        class="text-gray-500 hover:text-gray-700">
+                        <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 6L6 18" />
+                            <path d="M6 6l12 12" />
+                        </svg>
+                    </button>
                 </div>
-                @endforeach
+                @if ($images->count())
+                <div class="grid grid-cols-4 md:grid-cols-6 gap-4">
+                    @foreach ($images as $image)
+                    <div class="border border-gray-200 rounded p-1 shadow cursor-pointer"
+                        wire:click="selectImage({{ $image->id }})">
+                        <img src="{{ asset('storage/' . $image->image_name) }}" alt="{{ $image->id }} image"
+                            class="bg-stone-700 max-w-full h-25 object-cover rounded hover:bg-stone-400">
+                    </div>
+                    @endforeach
+                </div>
+                @else
+                <p class="text-gray-600">No logos available. Please upload a logo in the image gallery.</p>
+                @endif
             </div>
-            <div class="mt-4">
-                {{ $images->links() }}
-            </div>
-            @else
-            <p class="text-gray-600">No logos available. Please upload a logo in the image gallery.</p>
-            @endif
         </div>
     </div>
 </x-layouts.content>
