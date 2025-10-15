c296cbf86b99d8d3c162758c9b563028100eb859
hendradwiputra <hendra.doank@gmail.com>
2025-10-15 19:22:57 +0700

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,11 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Elequent\Relations\HasOne;
 
-class Image extends Model
+class Image extends Model        
 {
     protected $fillable = ['image_name'];
 
-    protected $table = 'images'; 
+    protected $table = 'images';                                
+
+    /**
+     * Get the slide images that use this image.
+     */
+    public function slideImages(): HasMany
+    {
+        return $this->hasMany(SlideImage::class, 'image_id');
+    }
+
+    /**
+     * Get the profile that uses this image.
+     */
+    public function profile():HasOne
+    {
+        return $this->hasOne(Profile::class, 'image_id');
+    }
 
 }
