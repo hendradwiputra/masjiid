19614676eb7d3884b844cd5e594c496851b59438
hendradwiputra <hendra.doank@gmail.com>
2025-10-17 18:57:41 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('storage/images/icon/hexagon-letter-m.svg') }}">
+    <link rel="stylesheet" href="{{ asset('storage/fonts/3d643187c5/fonts.css') }}">
+
     <title>{{ $title ?? 'Page title' }}</title>
     @livewireStyles
     @vite('resources/css/app.css', 'resources/js/app.js')
+
+    {{-- Loads Figtree --}}
+    @googlefonts
+
+    {{-- Loads Merriweather --}}
+    @googlefonts('merriweather')
+
+    {{-- Loads Nunito --}}
+    @googlefonts('nunito')
 
 </head>
 
