ebbab9f88b35f35e499f249a4524d8988ad54db3
hendradwiputra <hendra.doank@gmail.com>
2025-10-17 19:25:42 +0700

    add whitespace-nowrap in profile name

--- resources/views/livewire/praytimes/partials/profile-logo-left.blade.php
+++ resources/views/livewire/praytimes/partials/profile-logo-left.blade.php
@@ -1,8 +1,11 @@
 <div class="flex flex-row gap-3 items-center">
     <img id="logo" alt="" class="self-center flex-shrink-0 h-18 w-18 lg:h-26 lg:w-26">
     <div class="flex flex-col text-stone-200 text-xl text-shadow-lg mx-3">
-        <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me" id="name">
-        </h1>
+        <div class="flex flex-nowrap">
+            <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me whitespace-nowrap"
+                id="name">
+            </h1>
+        </div>
         <p class="text-sm lg:text-xl md:leading-5 lg:mb-2" id="address"></p>
         <p class="text-sm lg:text-xl md:leading-5" id="description"></p>
     </div>
--- resources/views/livewire/praytimes/partials/profile-logo-right.blade.php
+++ resources/views/livewire/praytimes/partials/profile-logo-right.blade.php
@@ -1,7 +1,10 @@
 <div class="flex flex-row gap-3 items-center">
     <div class="flex flex-col text-stone-200 text-right text-xl text-shadow-lg mx-3">
-        <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me" id="name">
-        </h1>
+        <div class="flex flex-nowrap">
+            <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me  whitespace-nowrap"
+                id="name">
+            </h1>
+        </div>
         <p class="text-sm lg:text-lg md:leading-5 lg:mb-2" id="address"></p>
         <p class="text-sm lg:text-lg md:leading-5" id="description"></p>
     </div>
