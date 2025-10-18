4d9ca99a4b519b104225fd17e0077d6857d76f2a
hendradwiputra <hendra.doank@gmail.com>
2025-10-18 16:46:25 +0700

    Update update-profile.blade.php

--- resources/views/livewire/profile/update-profile.blade.php
+++ resources/views/livewire/profile/update-profile.blade.php
@@ -97,15 +97,6 @@
                                 <div class="mt-2">
                                     <button type="button" wire:click="openImageModal"
                                         class="flex item-center px-2 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
-                                        <svg class="h-6 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M15 8h.01" />
-                                            <path d="M11 20h-4a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v4" />
-                                            <path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l3 3" />
-                                            <path d="M14 14l1 -1c.31 -.298 .644 -.497 .987 -.596" />
-                                            <path
-                                                d="M18.42 15.61a2.1 2.1 0 0 1 2.97 2.97l-3.39 3.42h-3v-3l3.42 -3.39z" />
-                                        </svg>
                                         Ubah Logo
                                     </button>
                                 </div>
@@ -181,35 +172,5 @@
     </div>
 
     <!-- Image Selection Modal -->
-    <div x-data="{ showImageModal: @entangle('showImageModal') }" x-show="showImageModal" x-cloak
-        class="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
-        <div class="bg-white rounded-lg p-6 w-full max-w-4xl">
-            <div class="overflow-y-auto max-h-[80vh]">
-                <div class="flex justify-between items-center mb-4">
-                    <h2 class="text-lg font-semibold">Pilih Logo</h2>
-                    <button @click="showImageModal = false" wire:click="closeImageModal"
-                        class="text-gray-500 hover:text-gray-700">
-                        <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M18 6L6 18" />
-                            <path d="M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                @if ($images->count())
-                <div class="grid grid-cols-4 md:grid-cols-6 gap-4">
-                    @foreach ($images as $image)
-                    <div class="border border-gray-200 rounded p-1 shadow cursor-pointer"
-                        wire:click="selectImage({{ $image->id }})">
-                        <img src="{{ asset('storage/' . $image->image_name) }}" alt="{{ $image->id }} image"
-                            class="bg-stone-700 max-w-full h-25 object-cover rounded hover:bg-stone-400">
-                    </div>
-                    @endforeach
-                </div>
-                @else
-                <p class="text-gray-600">No logos available. Please upload a logo in the image gallery.</p>
-                @endif
-            </div>
-        </div>
-    </div>
+    @include('livewire.image.image-modal')
 </x-layouts.content>
