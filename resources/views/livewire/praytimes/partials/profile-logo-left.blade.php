cd0a6ff6e47dfdcc7a8cde30c12d516c192e3744
hendradwiputra <hendra.doank@gmail.com>
2025-10-21 19:18:31 +0700

    Update profile-logo-left.blade.php

--- resources/views/livewire/praytimes/partials/profile-logo-left.blade.php
+++ resources/views/livewire/praytimes/partials/profile-logo-left.blade.php
@@ -2,7 +2,7 @@
     <img id="logo" alt="" class="self-center flex-shrink-0 h-18 w-18 lg:h-26 lg:w-26">
     <div class="flex flex-col text-stone-200 text-xl text-shadow-lg mx-3">
         <div class="flex flex-nowrap">
-            <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me whitespace-nowrap"
+            <h1 class="font-merriweather text-2xl lg:text-5xl md:leading-10 font-bold lg:mb-3 highlight-me md:whitespace-nowrap"
                 id="name">
             </h1>
         </div>
