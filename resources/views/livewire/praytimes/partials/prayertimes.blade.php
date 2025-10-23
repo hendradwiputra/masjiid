36de4dff8acc93de4460aba795bc3e7116563c55
hendradwiputra <hendra.doank@gmail.com>
2025-10-23 15:26:38 +0700

    Update prayertimes.blade.php

--- resources/views/livewire/praytimes/partials/prayertimes.blade.php
+++ resources/views/livewire/praytimes/partials/prayertimes.blade.php
@@ -2,8 +2,10 @@
     class="flex text-stone-200 lg:text-xl rounded-lg bg-gradient-to-b from-stone-800 to to-stone-400 md:py-2"
     id="{{ $i }}">
     <div class="mx-auto">
-        <h4 class="font-semibold text-xl lg:text-4xl mb-1" id="praynames{{ $i }}"></h4>
-        <h3 class="font-bold text-2xl lg:text-7xl leading-4 lg:leading-14 mb-1" id="praytimes{{ $i }}">
+        <h4 class="font-semibold text-xl lg:text-4xl text-shadow-lg text-shadow-gray-700 mb-1" id="praynames{{ $i }}">
+        </h4>
+        <h3 class="font-bold text-2xl lg:text-7xl leading-4 lg:leading-14 text-shadow-lg text-shadow-gray-700 mb-1"
+            id="praytimes{{ $i }}">
         </h3>
     </div>
     </div>
