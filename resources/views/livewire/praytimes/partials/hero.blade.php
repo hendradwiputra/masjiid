33354112847f0829747cb7317d3ffcd185f68231
hendradwiputra <hendra.doank@gmail.com>
2025-10-23 15:23:26 +0700

    Update hero.blade.php

--- resources/views/livewire/praytimes/partials/hero.blade.php
+++ resources/views/livewire/praytimes/partials/hero.blade.php
@@ -6,27 +6,28 @@
             alt="Random Background Image" id="random-image"
             class="w-full h-full object-cover object-center transition-opacity duration-700 ease-in-out">
         @endif
-        <div class="absolute inset-0 bg-stone-950 opacity-10"></div> <!-- Slightly darker for contrast -->
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <!-- Slightly darker for contrast -->
     </div>
 
     <!-- CENTERED METADATA -->
     @if(!empty($randomImages) && is_array($randomImages) && !empty($randomImages[0]['url']))
     <div class="absolute inset-0 flex items-center justify-center z-40 pointer-events-none">
-        <div
-            class="text-center p-4 md:p-6 bg-white/30 backdrop-blur-xs bg-opacity-70 text-white max-w-screen mx-4 rounded-2xl">
-            <h2 id="slide-title"
-                class="text-gray-900 text-2xl md:text-4xl lg:text-7xl font-bold mb-3 leading-tight text-shadow-lg text-shadow-gray-200">
-                {{ $randomImages[0]['title'] ?? 'No Title' }}
+        <div class="text-center p-4 md:p-6 bg-opacity-70 max-w-7xl">
+            <h2 id="slide-title" class="font-bold text-gray-300 text-2xl md:text-4xl lg:text-7xl leading-tight text-shadow-lg
+                   text-shadow-gray-900 mb-3">
+                {{ $randomImages[0]['title'] ?? '' }}
             </h2>
-            <p id="slide-content"
-                class="text-gray-100 text-base md:text-2xl lg:text-5xl font-semibold mb-2 leading-none mx-auto text-shadow-lg text-shadow-gray-800">
-                {{ $randomImages[0]['content'] ?? 'No Content' }}
+            <p id="slide-content" class="font-semibold text-amber-50 text-base md:text-2xl lg:text-5xl text-shadow-lg text-shadow-gray-900 leading-none 
+                 mb-2 mx-auto">
+                {{ $randomImages[0]['content'] ?? '' }}
             </p>
-            <p id="slide-author"
-                class="text-gray-100 text-sm md:text-xl font-semibold italic text-shadow-lg text-shadow-gray-800">
-                — {{ $randomImages[0]['author'] ?? 'Unknown Author' }}
+            <p id="slide-author" class="font-semibold text-amber-50 text-sm md:text-xl italic text-shadow-lg text-shadow-gray-900
+                mb-2 mx-auto">
+                — {{ $randomImages[0]['author'] ?? '' }}
             </p>
         </div>
     </div>
     @endif
+
 </div>
