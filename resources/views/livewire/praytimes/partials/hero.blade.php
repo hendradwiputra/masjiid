90ab717d105e3177d63230d37cfaa4241f8cc634
hendradwiputra <hendra.doank@gmail.com>
2025-10-23 15:32:11 +0700

    Update hero.blade.php

--- resources/views/livewire/praytimes/partials/hero.blade.php
+++ resources/views/livewire/praytimes/partials/hero.blade.php
@@ -2,7 +2,7 @@
     <!-- Background Image -->
     <div class="absolute inset-0 bg-gradient-to-b from-gray-900 to bg-gray-400 overflow-hidden">
         @if(!empty($randomImages) && is_array($randomImages) && !empty($randomImages[0]['url']))
-        <img src="{{ $randomImages[0]['url'] ?? asset('storage/images/upload/default-image.jpg') }}"
+        <img src="{{ $randomImages[0]['url'] ?? asset('storage/images/upload/default-image.webp') }}"
             alt="Random Background Image" id="random-image"
             class="w-full h-full object-cover object-center transition-opacity duration-700 ease-in-out">
         @endif
