e7fc3bb987cc86fdf522716eef5f2d9a72bb750c
hendradwiputra <hendra.doank@gmail.com>
2025-10-30 17:37:07 +0700

    Add prayertimes icons

--- app/Livewire/Slide/PraytimeSlide.php
+++ app/Livewire/Slide/PraytimeSlide.php
@@ -20,6 +20,7 @@ class PraytimeSlide extends Component
     public $praytimes;
     public $randomImages = [];
     public $logo;
+    public $prayerIcons;
     public $tickerText = '';
 
     #[Title('Masjiid')]
@@ -53,6 +54,7 @@ class PraytimeSlide extends Component
     public function getPraytimes()
     {
         return Cache::remember('praytimes', 300, function () {
+
             $praytimes = Praytime::first() ?? new Praytime(); // Fallback to empty model
 
             // Sanitize fields to prevent JSON issues
@@ -96,6 +98,119 @@ class PraytimeSlide extends Component
 
             return $sanitized;
         });
+    }
+
+    public function getPrayerIcons()
+    {
+        return [
+                    1 => '
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="h-10 w-10"
+                            >
+                            <path d="M3 13h1" />
+                            <path d="M20 13h1" />
+                            <path d="M5.6 6.6l.7 .7" />
+                            <path d="M18.4 6.6l-.7 .7" />
+                            <path d="M8 13a4 4 0 1 1 8 0" />
+                            <path d="M3 17h18" />
+                            <path d="M7 20h5" />
+                            <path d="M16 20h1" />
+                            <path d="M12 5v-1" />
+                        </svg>',
+                    
+                    2 => '
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="h-10 w-10"
+                            >
+                            <path d="M3 17h1m16 0h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7m-9.7 5.7a4 4 0 0 1 8 0" />
+                            <path d="M3 21l18 0" />
+                            <path d="M12 9v-6l3 3m-6 0l3 -3" />
+                        </svg>',
+                    
+                    3 => '
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="h-10 w-10"
+                            >
+                            <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" />
+                            <path d="M6.343 17.657l-1.414 1.414" />
+                            <path d="M6.343 6.343l-1.414 -1.414" />
+                            <path d="M17.657 6.343l1.414 -1.414" />
+                            <path d="M17.657 17.657l1.414 1.414" />
+                            <path d="M4 12h-2" />
+                            <path d="M12 4v-2" />
+                            <path d="M20 12h2" />
+                            <path d="M12 20v2" />
+                        </svg>',
+                    
+                    4 => '
+                    <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="h-10 w-10"
+                        >
+                        <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                        <path d="M4 12h.01" />
+                        <path d="M12 4v.01" />
+                        <path d="M20 12h.01" />
+                        <path d="M12 20v.01" />
+                        <path d="M6.31 6.31l-.01 -.01" />
+                        <path d="M17.71 6.31l-.01 -.01" />
+                        <path d="M17.7 17.7l.01 .01" />
+                        <path d="M6.3 17.7l.01 .01" />
+                    </svg>',
+                    
+                    5 => '
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="h-10 w-10"
+                            >
+                            <path d="M3 17h1m16 0h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7m-9.7 5.7a4 4 0 0 1 8 0" />
+                            <path d="M3 21l18 0" />
+                            <path d="M12 3v6l3 -3m-6 0l3 3" />
+                        </svg>',
+                    
+                    6 => '
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="h-10 w-10"
+                            >
+                            <path d="M3 16h18" />
+                            <path d="M3 20h18" />
+                            <path d="M8.296 16c-2.268 -1.4 -3.598 -4.087 -3.237 -6.916c.443 -3.48 3.308 -6.083 6.698 -6.084v.006h.296c-1.991 1.916 -2.377 5.03 -.918 7.405c1.459 2.374 4.346 3.33 6.865 2.275a6.888 6.888 0 0 1 -2.777 3.314" />
+                        </svg>'
+                ];
     }
 
     protected function loadRandomImages()
@@ -180,7 +295,8 @@ class PraytimeSlide extends Component
             \Log::info('Current time in WIB:', ['now' => $now->toDateTimeString()]);
 
             // Fetch active announcements
-            $announcements = RunningText::where('status_id', 1)
+            $announcements = RunningText::orderBy('created_at')
+                ->where('status_id', 1)
                 ->where('start_date', '<=', $now)
                 ->where('end_date', '>=', $now)
                 ->pluck('announcement')
@@ -202,6 +318,7 @@ class PraytimeSlide extends Component
     {
         $this->profile = $this->getprofile();      
         $this->praytimes = $this->getPraytimes();
+        $this->prayerIcons = $this->getPrayerIcons();
         $this->loadRandomImages();
         $this->updateTickerText();
     }
@@ -218,6 +335,7 @@ class PraytimeSlide extends Component
             'praytimes' => $this->praytimes,
             'tickerText' => $this->tickerText,
             'randomImages' => $this->randomImages,
+            'prayerIcons' => $this->prayerIcons,
         ]);
     }
 }
