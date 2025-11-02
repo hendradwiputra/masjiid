8c0fd859a403896923244ec6ed66fb962f2b7118
hendradwiputra <hendra.doank@gmail.com>
2025-11-02 17:31:31 +0700

    Update theme2.blade.php

--- resources/views/livewire/praytimes/themes/theme2.blade.php
+++ resources/views/livewire/praytimes/themes/theme2.blade.php
@@ -1,60 +1,53 @@
-<div class="flex flex-col lg:flex-row min-h-screen">
-    <!-- Left Section -->
-    <div class="w-full lg:w-1/2 bg-gradient-to-l from-gray-800 to-gray-600">
+<div class="h-screen bg-gradient-to-r from-gray-600 to-gray-800 text-white overflow-hidden flex">
+
+    <!-- Left Side -->
+    <div class="w-full lg:w-1/2 flex flex-col relative">
+
         <!-- Profile -->
-        <div class="w-full p-5 transition-all duration-700 ease-in-out">
-            <div class="max-w-3xl mx-auto">
-                <div class="flex flex-row gap-2 items-center justify-center">
-                    <x-partials.logo />
-                    <div class="flex flex-col text-stone-200 text-xl text-shadow-lg">
-                        <h1 class="font-merriweather font-bold text-2xl lg:text-3xl md:leading-8 lg:mb-2 highlight-me"
-                            id="name">
-                        </h1>
-                        <p class="text-sm lg:text-xl md:leading-5 lg:mb-2" id="address"></p>
-                        <p class="text-sm lg:text-xl md:leading-5" id="description"></p>
-                    </div>
-                </div>
+        <div class="relative p-6 text-center">
+            <div class="flex flex-col text-stone-200 text-xl text-shadow-lg">
+                <h1 class="font-merriweather font-bold text-2xl lg:text-3xl md:leading-8 lg:mb-2 highlight-me"
+                    id="name">
+                </h1>
+                <p class="text-sm lg:text-xl md:leading-5 lg:mb-2" id="address"></p>
+                <p class="text-sm lg:text-xl md:leading-5" id="description"></p>
             </div>
         </div>
 
         <!-- Current Date -->
-        <div class="p-2 mb-5 text-right border-t-2 border-gray-600">
-            <div class="flex flex-col">
-                <div class="font-semibold text-stone-200 text-md lg:text-xl text-shadow-lg mr-1">
-                    <x-partials.currentdate />
-                </div>
-            </div>
+        <div class="relative text-center font-semibold text-stone-200 text-md lg:text-xl">
+            <x-partials.currentdate />
         </div>
 
-        <!-- Prayer Times -->
-        <div class="mx-4">
-            <div class="font-nunito text-white">
-                <div class="grid grid-cols-1 gap-2 md:gap-4">
-                    @for ($i=1; $i<=6; $i++) <div
-                        class="prayer-card bg-gradient-to-r from-blue-500 to-blue-400 rounded-full px-5 p-1 lg:p-3 flex items-center justify-between shadow-md hover:shadow-lg transition-all duration-300"
-                        id="{{ $i }}">
-                        <div class="flex items-center space-x-2 md:space-x-3">
-                            <div class="flex-shrink-0">
-                                {!! $prayerIcons[$i] ?? '' !!}
-                            </div>
-                            <div class="min-w-0 flex-1">
-                                <h3 class="text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold truncate"
-                                    id="praynames{{ $i }}"></h3>
-                            </div>
+        <!-- Praytimes -->
+        <div class="relative flex-1 flex items-center justify-center px-8 py-4">
+            <div class="w-full h-full space-y-4">
+                @for ($i=1; $i<=6; $i++) <div
+                    class="bg-white/10 backdrop-blur rounded-2xl px-6 py-4 flex justify-between items-center"
+                    id="{{ $i }}">
+                    <div class="flex items-center space-x-2 md:space-x-3">
+                        <div class="flex-shrink-0">
+                            {!! $prayerIcons[$i] ?? '' !!}
                         </div>
-                        <div class="text-lg sm:text-xl md:text-2xl lg:text-4xl font-bold ml-2 md:ml-4 whitespace-nowrap"
-                            id="praytimes{{ $i }}"></div>
-                </div>
-                @endfor
+                        <div class="min-w-0 flex-1">
+                            <h3 class="text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold truncate"
+                                id="praynames{{ $i }}"></h3>
+                        </div>
+                    </div>
+                    <div class="text-lg sm:text-xl md:text-2xl lg:text-4xl font-bold ml-2 md:ml-4 whitespace-nowrap"
+                        id="praytimes{{ $i }}"></div>
             </div>
+            @endfor
         </div>
     </div>
+
 </div>
 
-<!-- Right Section -->
-<div class="w-full lg:w-1/2 relative min-h-[500px] lg:min-h-screen hidden lg:block">
+<!-- Right Side -->
+<div class="hidden lg:block w-1/2 relative overflow-hidden">
+
     <!-- Next Prayer -->
-    <div class="z-50 fixed right-1 pt-2">
+    <div class="z-50 fixed right-1 pt-2 hidden lg:flex">
         <div class="flex items-center">
             <div class="bg-gradient-to-b from-teal-500 to-teal-700 outline-2 outline-stone-50 py-1 px-3 rounded-l-full">
                 <svg viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
@@ -75,19 +68,20 @@
         </div>
     </div>
 
-    <!-- Hero section -->
-    <div class="w-full h-full">
-        <x-partials.hero-simple-centered :randomImages="$randomImages" />
+    <!-- Hero -->
+    <div id="hero-section">
+        <x-partials.hero-with-text-small-screen :randomImages="$randomImages" />
     </div>
 
     <!-- Clock -->
-    <div class="fixed bottom-20 right-4 lg:right-1 z-10">
-        <div class="flex flex-row bg-gradient-to-r from-stone-400 to-stone-200 rounded-sm shadow-lg p-2">
+    <div class="fixed bottom-23 right-4 lg:right-1 z-10 hidden lg:block">
+        <div class="flex flex-row bg-gradient-to-r from-stone-400 to-stone-200 rounded-lg shadow-lg p-2">
             <x-partials.clock />
         </div>
     </div>
 </div>
 
+<!-- Footer -->
 <div class="fixed inset-x-0 bottom-0 bg-gradient-to-r from-gray-800 to-gray-900 z-50">
     <!-- Running Text Section -->
     <x-partials.running-text :tickerText="$tickerText" />
