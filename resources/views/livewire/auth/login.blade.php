7f4d5de16f779a761525805cb0a949ec08e40830
hendradwiputra <hendra.doank@gmail.com>
2025-11-02 17:30:59 +0700

    Update login.blade.php

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -21,7 +21,7 @@
             <form class="space-y-4" wire:submit="login">
 
                 <div>
-                    <label for="new-password" class="block text-sm font-medium text-gray-700">Name</label>
+                    <label for="new-password" class="block text-base font-medium text-gray-700">Username</label>
                     <div class="mt-1">
                         <input name="name" type="name"
                             class="@error('name') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
@@ -33,7 +33,7 @@
                 </div>
 
                 <div>
-                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <label for="password" class="block text-base font-medium text-gray-700">Password</label>
                     <div class="mt-1">
                         <input name="password" type="password"
                             class="@error('password') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
@@ -46,7 +46,7 @@
 
                 <div>
                     <button type="submit" wire:loading.attr="disabled" wire:loading.class="opacity-75"
-                        class="flex w-full justify-center rounded-full border border-transparent bg-blue-500 hover:bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">
+                        class="flex w-full justify-center rounded-full border border-transparent bg-blue-500 hover:bg-blue-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">
                         <span wire:loading.remove>Login</span>
                         <span wire:loading>Processing...</span>
                     </button>
