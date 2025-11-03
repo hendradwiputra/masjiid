9ba61b32451e72eef2ac4687643634a8064a16c8
hendradwiputra <hendra.doank@gmail.com>
2025-11-03 10:51:02 +0700

    Update theme1.blade.php

--- resources/views/livewire/praytimes/themes/theme1.blade.php
+++ resources/views/livewire/praytimes/themes/theme1.blade.php
@@ -2,7 +2,7 @@
 <div class="fixed left-0 inset-x-0 top-0 z-50 transition-all duration-700 ease-in-out">
     <div id="date-section">
         <!-- Current Date -->
-        <div id="datetime_section" class="bg-gradient-to-r from-gray-900 to-gray-600">
+        <div id="datetime_section" class="bg-gradient-to-l from-blue-950 to-blue-400">
             <div class="mx-auto w-full py-1 px-1 text-right">
                 <div class="flex flex-col">
                     <div class="font-semibold text-stone-200 text-md lg:text-2xl text-shadow-lg">
@@ -14,7 +14,7 @@
     </div>
 
     <div id="profile-section"
-        class="lg:absolute bg-gradient-to-l from-gray-800 to-gray-600 shadow-xl top-8 lg:top-0 p-4 z-50 lg:rounded-br-7xl w-full lg:max-w-fit transition-all duration-700 ease-in-out">
+        class="lg:absolute bg-gradient-to-r from-gray-900 to-gray-400 shadow-xl top-8 lg:top-0 p-4 z-50 lg:rounded-br-7xl w-full lg:max-w-fit transition-all duration-700 ease-in-out">
         <!-- Profile & Logo -->
         <div class="flex flex-row gap-3 items-center">
             <x-partials.logo />
@@ -30,24 +30,7 @@
 
     <div id="nextprayer-section" class="z-50 fixed right-1 pt-2">
         <!-- Next Prayer -->
-        <div class="flex items-center">
-            <div class="bg-gradient-to-b from-teal-500 to-teal-700 outline-2 outline-stone-50 py-1 px-3 rounded-l-full">
-                <svg viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="animate-pulse h-7 lg:h-20 max-w-[30px] max-h-[30px] lg:max-w-[40px] lg:max-h-[48px]">
-                    <path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5" />
-                    <path d="M5.63 7.16l0 .01" />
-                    <path d="M4.06 11l0 .01" />
-                    <path d="M4.63 15.1l0 .01" />
-                    <path d="M7.16 18.37l0 .01" />
-                    <path d="M11 19.94l0 .01" />
-                </svg>
-            </div>
-            <div
-                class="font-medium text-gray-800 text-xl lg:text-5xl tracking-tighter tabular-nums inline-flex flex-wrap items-center justify-center gap-x-2 bg-gradient-to-b from-stone-50 to-stone-300 outline-2 outline-stone-50 shadow-xl py-1 px-3 rounded-r-full whitespace-nowrap">
-                <x-partials.nextprayer />
-            </div>
-        </div>
+        <x-partials.nextprayer />
     </div>
 </div>
 <!-- End of header -->
@@ -60,9 +43,9 @@
 <!-- Footer -->
 <div class="fixed inset-x-0 bottom-0 z-50 mx-auto w-full">
 
-    <div id="clock-section" class="flex justify-start mb-1">
+    <div id="clock-section" class="flex justify-start mb-2">
         <!-- Clock -->
-        <div class="flex flex-row bg-gradient-to-r from-stone-400 to bg-stone-200 rounded-lg mx-1">
+        <div class="flex flex-row bg-white/30 backdrop-blur rounded-lg mx-1">
             <x-partials.clock />
         </div>
     </div>
