e1ded70d76e7499dbb1db063be9ae372f86cc090
hendradwiputra <hendra.doank@gmail.com>
2025-11-03 16:27:41 +0700

    Update login.blade.php

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -18,16 +18,16 @@
             </div>
             @endif
 
-            <form class="space-y-4" wire:submit="login">
+            <form class="space-y-6" wire:submit="login">
 
                 <div>
                     <label for="new-password" class="block text-base font-medium text-gray-700">Username</label>
                     <div class="mt-1">
                         <input name="name" type="name"
-                            class="@error('name') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                            class="@error('name') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500"
                             id="name" wire:model="name" />
                         @if ($errors->has('name'))
-                        <span class="text-red-500 text-sm">{{ $errors->first('name') }}</span>
+                        <span class="text-red-500 text-base">{{ $errors->first('name') }}</span>
                         @endif
                     </div>
                 </div>
@@ -36,25 +36,21 @@
                     <label for="password" class="block text-base font-medium text-gray-700">Password</label>
                     <div class="mt-1">
                         <input name="password" type="password"
-                            class="@error('password') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                            class="@error('password') is-invalid @enderror px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500"
                             id="password" wire:model="password" />
                         @if ($errors->has('password'))
-                        <span class="text-red-500 text-sm">{{ $errors->first('password') }}</span>
+                        <span class="text-red-500 text-base">{{ $errors->first('password') }}</span>
                         @endif
                     </div>
                 </div>
 
                 <div>
                     <button type="submit" wire:loading.attr="disabled" wire:loading.class="opacity-75"
-                        class="flex w-full justify-center rounded-full border border-transparent bg-blue-500 hover:bg-blue-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">
+                        class="flex w-full justify-center rounded-full border border-transparent bg-blue-500 hover:bg-blue-600 py-3 px-4 text-md font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">
                         <span wire:loading.remove>Login</span>
                         <span wire:loading>Processing...</span>
                     </button>
                 </div>
-                <p class="text-center text-sm text-gray-900">
-                    Tidak punya akun? Buat akun baru <a href="{{ route('register') }}"
-                        class="text-blue-500 hover:underline font-semibold">di sini</a>
-                </p>
             </form>
         </div>
     </div>
