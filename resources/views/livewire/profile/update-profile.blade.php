2b957276bd94c8158d5829b90d64eab6ed13b10d
hendradwiputra <hendra.doank@gmail.com>
2025-11-03 16:27:51 +0700

    Update update-profile.blade.php

--- resources/views/livewire/profile/update-profile.blade.php
+++ resources/views/livewire/profile/update-profile.blade.php
@@ -115,7 +115,7 @@
                             <select wire:model.live="selected_theme"
                                 class="text-base lg:text-base bg-stone-100 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="">Pilih tema</option>
-                                @for($i = 1; $i <= 4; $i++) <option value="theme{{ $i }}">Theme{{ $i }}</option>
+                                @for($i = 1; $i <= 3; $i++) <option value="theme{{ $i }}">Theme{{ $i }}</option>
                                     @endfor
                             </select>
                             @error('selected_theme') <span class="text-red-500 text-base">{{ $message }}</span>
