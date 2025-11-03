add809f71680445703c7dbd6efdf265614076aa6
hendradwiputra <hendra.doank@gmail.com>
2025-11-03 10:50:59 +0700

    Update nextprayer.blade.php

--- resources/views/components/partials/nextprayer.blade.php
+++ resources/views/components/partials/nextprayer.blade.php
@@ -1,3 +1,18 @@
-<div id="nextPrayName">
-    00:00:00
+<div class="flex items-center">
+    <div class="bg-gradient-to-b from-teal-500 to-teal-700 outline-2 outline-stone-50 py-1 px-3 rounded-l-full">
+        <svg viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round"
+            class="animate-pulse h-7 lg:h-20 max-w-[30px] max-h-[30px] lg:max-w-[40px] lg:max-h-[48px]">
+            <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+            <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+            <path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
+            <path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
+        </svg>
+    </div>
+    <div
+        class="font-medium text-gray-800 text-xl lg:text-5xl tracking-tighter tabular-nums inline-flex flex-wrap items-center justify-center gap-x-2 bg-gradient-to-b from-stone-50 to-stone-300 outline-2 outline-stone-50 shadow-xl py-1 px-3 rounded-r-full whitespace-nowrap">
+        <div id="nextPrayName">
+            00:00:00
+        </div>
+    </div>
 </div>
