fb309b0f505da92ce8d71e932bc19280feabc2d5
hendradwiputra <hendra.doank@gmail.com>
2025-11-04 21:22:05 +0700

    Update show-running-text.blade.php

--- resources/views/livewire/running-text/show-running-text.blade.php
+++ resources/views/livewire/running-text/show-running-text.blade.php
@@ -110,7 +110,7 @@
 
     <!-- Create/Edit Modal -->
     <div x-data="{ showModal: @entangle('showModal') }" x-show="showModal" x-cloak
-        class="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50 backdrop-blur-sm">
+        class="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
         <div class="bg-white rounded-lg p-6 w-full max-w-xl">
             <div class="flex justify-between items-center mb-4">
                 <h2 class="flex items-center text-lg font-semibold">
