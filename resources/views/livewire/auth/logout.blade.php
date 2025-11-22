58a254617a7583be159b64526b736130014ee161
hendradwiputra <hendra.doank@gmail.com>
2025-11-22 10:35:06 +0700

    Update logout.blade.php

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button wire:click="logout"
-        class="flex items-center w-full text-left px-2 py-2 gap-2 tracking-wide text-base text-red-600 font-semibold hover:bg-red-100">
+        class="flex items-center w-full text-left px-4 py-2 gap-2 text-base text-red-600 font-semibold hover:bg-red-50">
         Logout
     </button>
 
