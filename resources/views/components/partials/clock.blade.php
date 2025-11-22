5c7a08b377a643d3b9125943ca853644cbaaf910
hendradwiputra <hendra.doank@gmail.com>
2025-11-22 10:34:56 +0700

    Update clock.blade.php

--- resources/views/components/partials/clock.blade.php
+++ resources/views/components/partials/clock.blade.php
@@ -1,10 +1,10 @@
 <div class="flex items-center justify-center space-x-2 max-w-full overflow-hidden">
-    <h2 class="text-6xl lg:text-8xl font-bold text-gray-800 text-shadow-lg text-shadow-white mx-2 truncate" id="clock">
+    <h2 class="text-6xl lg:text-8xl font-bold text-gray-800 text-shadow-lg text-shadow-white truncate" id="clock">
     </h2>
     <div class="flex flex-col items-start">
-        <h4 class="font-bold text-teal-800 text-2xl lg:text-5xl text-shadow-lg text-shadow-white tabular-nums"
+        <h4 class="font-bold text-teal-800 text-2xl lg:text-4xl text-shadow-lg text-shadow-white tabular-nums"
             id="seconds"></h4>
-        <h4 class="font-bold text-teal-800 text-2xl lg:text-4xl text-shadow-lg text-shadow-white leading-none"
+        <h4 class="font-bold text-teal-800 text-2xl lg:text-3xl text-shadow-lg text-shadow-white leading-none"
             id="ampm">
         </h4>
     </div>
