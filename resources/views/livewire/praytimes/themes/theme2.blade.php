b569e6446966ecb4cafb9b23110215bac21106e7
hendradwiputra <hendra.doank@gmail.com>
2025-11-23 15:08:26 +0700

    Update theme2.blade.php

--- resources/views/livewire/praytimes/themes/theme2.blade.php
+++ resources/views/livewire/praytimes/themes/theme2.blade.php
@@ -1,65 +1,87 @@
-<div class="h-screen bg-gradient-to-r from-gray-900 to-gray-400 text-white overflow-hidden flex">
+<div class="h-screen bg-gradient-to-r from-gray-900 to-gray-400 text-white overflow-hidden flex flex-col">
+    <!-- Main Content Area -->
+    <div class="flex-1 flex overflow-hidden">
 
-    <!-- Left Side -->
-    <div class="w-full lg:w-1/2 flex flex-col relative">
+        <!-- Left Side -->
+        <div class="w-full lg:w-1/2 flex flex-col relative">
 
-        <!-- Profile -->
-        <div class="relative p-5 text-center">
-            <div class="flex flex-col text-stone-200 text-xl text-shadow-lg">
-                <h1 class="font-merriweather font-bold text-xl lg:text-3xl md:leading-8 lg:mb-2 highlight-me" id="name">
-                </h1>
-                <p class="text-base md:text-lg lg:text-xl md:leading-5 lg:mb-2" id="address"></p>
-                <p class="text-base md:text-lg lg:text-xl md:leading-5" id="description"></p>
+            <!-- Praytimes Section -->
+            <div class="relative flex-1 flex items-center justify-center px-8 py-2 pb-22">
+
+                <div class="w-full h-full grid grid-rows-6 gap-3">
+                    @for ($i=1; $i<=6; $i++) <div
+                        class="bg-white/10 backdrop-blur rounded-2xl px-6 flex justify-between items-center relative"
+                        id="{{ $i }}">
+                        <div class="flex items-center space-x-2 md:space-x-3 flex-1">
+                            <div class="flex-shrink-0">
+                                {!! $prayerIcons[$i] ?? '' !!}
+                            </div>
+                            <div class="min-w-0 flex-1">
+                                <h3 class="text-xl md:text-2xl lg:text-4xl text-shadow-sm text-shadow-gray-800 font-semibold"
+                                    id="praynames{{ $i }}"></h3>
+                            </div>
+                        </div>
+
+                        <!-- Centered dotted circle -->
+                        <div
+                            class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+                            <div class="flex size-3 space-x-1">
+                                <span class="size-3 bg-white rounded-full"></span>
+                            </div>
+                        </div>
+
+                        <div class="text-lg sm:text-xl md:text-2xl lg:text-6xl text-shadow-sm text-shadow-gray-800 font-bold ml-2 md:ml-4 whitespace-nowrap flex-1 text-right"
+                            id="praytimes{{ $i }}"></div>
+                </div>
+                @endfor
+
+                <!-- Next Prayer Section -->
+                <div id="nextprayer-section" class="flex justify-center items-center">
+                    <x-partials.nextprayer-2 />
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Right Side -->
+    <div class="hidden lg:block w-1/2 relative overflow-hidden">
+        <!-- Hero -->
+        <div id="hero-section" class="relative w-full h-full overflow-hidden">
+            <x-partials.hero-with-video :videoPath="$videoPath" />
+        </div>
+
+        {{-- Header --}}
+        <div id="profile-section" class="flex justify-end items-center absolute top-0 right-0 p-2 bg-gradient-to-r from-black/70 via-transparent to-black/70
+                    backdrop-blur-sm border-b border-white/10 w-full">
+
+            {{-- Profile --}}
+            <div class="flex w2/3 items-center justify-end gap-4">
+                <div class="text-right">
+                    <h1 class="font-merriweather text-2xl lg:text-3xl font-bold drop-shadow-md highlight-me" id="name">
+                    </h1>
+                    <p class="text-base lg:text-lg" id="address"></p>
+                    <p class="text-base lg:text-lg" id="description"></p>
+                </div>
+                <div class="flex-shrink-0">
+                    <x-partials.logo />
+                </div>
+            </div>
+
+            {{-- Current Date --}}
+            <div class="absolute -bottom-8 left-0 right-0 bg-gradient-to-r from-blue-950/80 to-blue-700/80 
+                          backdrop-blur-sm border-t border-indigo-300/30">
+                <div class="text-right font-medium text-lg lg:text-2xl drop-shadow px-2">
+                    <x-partials.currentdate />
+                </div>
             </div>
         </div>
 
-        <!-- Current Date -->
-        <div
-            class="relative text-center font-semibold text-stone-200 text-md md:text-lg lg:text-xl bg-gradient-to-r from-blue-950 to-blue-400 py-1 mb-2">
-            <x-partials.currentdate />
-        </div>
 
-        <!-- Praytimes -->
-        <div class="relative flex-1 flex items-center justify-center px-8 py-4">
-            <div class="w-full h-full space-y-4">
-                @for ($i=1; $i<=6; $i++) <div
-                    class="bg-white/10 backdrop-blur rounded-2xl px-6 py-1 md:py-2 lg:py-4 flex justify-between items-center"
-                    id="{{ $i }}">
-                    <div class="flex items-center space-x-2 md:space-x-3">
-                        <div class="flex-shrink-0">
-                            {!! $prayerIcons[$i] ?? '' !!}
-                        </div>
-                        <div class="min-w-0 flex-1">
-                            <h3 class="text-xl md:text-2xl lg:text-4xl font-semibold truncate" id="praynames{{ $i }}">
-                            </h3>
-                        </div>
-                    </div>
-                    <div class="text-lg sm:text-xl md:text-2xl lg:text-4xl font-bold ml-2 md:ml-4 whitespace-nowrap"
-                        id="praytimes{{ $i }}"></div>
-            </div>
-            @endfor
-        </div>
-    </div>
 
-</div>
-
-<!-- Right Side -->
-<div class="hidden lg:block w-1/2 relative overflow-hidden">
-
-    <!-- Next Prayer -->
-    <div id="nextprayer-section" class="z-50 fixed right-1 pt-2 hidden lg:flex">
-        <x-partials.nextprayer />
-    </div>
-
-    <!-- Hero -->
-    <div id="hero-section">
-        <x-partials.hero-with-text-small-screen :randomImages="$randomImages" />
-    </div>
-
-    <!-- Clock -->
-    <div id="clock-section" class="fixed bottom-23 right-4 lg:right-1 z-10 hidden lg:block">
-        <div class="flex flex-row bg-white/30 backdrop-blur rounded-lg shadow-lg p-0">
-            <x-partials.clock />
+        <!-- Clock -->
+        <div id="clock-section" class="fixed bottom-23 right-4 lg:right-1 px-1 z-10 hidden lg:block">
+            <x-partials.clock-1 />
         </div>
     </div>
 </div>
@@ -73,5 +95,5 @@
     <div id="copyright-section">
         <x-partials.copyright />
     </div>
-
+</div>
 </div>
